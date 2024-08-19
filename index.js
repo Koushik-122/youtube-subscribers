a78e3dc7e64d257a1f87f8db31f5ef46c973b379
Koushik-122 <147714693+Koushik-122@users.noreply.github.com>
2024-08-19 14:06:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 require("dotenv").config();
 
 // Connect to DATABASE
-const DATABASE_URL = "process.env.DATABASE_URL"
+const DATABASE_URL = process.env.DATABASE_URL
 mongoose.connect(DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
